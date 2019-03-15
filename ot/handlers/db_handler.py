ec335c88b72a012687a845ab30ee2bced17f5673
William.McGuinness <William.McGuinness@am.jll.com>
2019-03-15 10:47:51 -0500

    adding full db export method to sqlite db handler

--- ot/handlers/db_handler.py
+++ ot/handlers/db_handler.py
@@ -108,6 +108,31 @@ class SQLiteHandler(object):
         out_df = pd.read_sql(query, conn)
         return out_df
 
+    def export_db_to_excel(self, filename):
+        all_tables = self.sql_to_df(
+            query="""
+                SELECT name 
+                FROM sqlite_master 
+                WHERE type='table' 
+            """
+        )
+        all_tables = all_tables['name'].tolist()
+        df_dict = {}
+        for table in all_tables:
+            df = self.sql_to_df(
+                query="""
+                    SELECT *
+                    FROM {}
+                """.format(table)
+            )
+            df_dict[table] = df
+
+        writer = pd.ExcelWriter(filename)
+        for table, df in df_dict.items():
+            df.to_excel(writer, sheet_name=table)
+
+        writer.save()
+
 
 if __name__ == '__main__':
     with SQLiteHandler(db_name='test.db') as h:
--- sandbox/bm/poker_atlas_sandbox.py
+++ sandbox/bm/poker_atlas_sandbox.py
@@ -2,32 +2,11 @@ import ot
 import pandas as pd
 
 def run_job():
-    df_dict = {}
     with ot.SQLiteHandler('onetime.db') as s:
-        all_tables = s.sql_to_df(
-            query="""
-                SELECT name 
-                FROM sqlite_master 
-                WHERE type='table' 
-            """
+        s.export_db_to_excel(
+            'C:/users/william.mcguinness/scratch/onetime_db.xlsx'
         )
-        all_tables = all_tables['name'].tolist()
-        for table in all_tables:
-            df = s.sql_to_df(
-                query="""
-                    SELECT *
-                    FROM {}
-                """.format(table)
-            )
-            df_dict[table] = df
 
-    writer = pd.ExcelWriter(
-        'C:/users/william.mcguinness/scratch/onetime_db.xlsx'
-    )
-    for table, df in df_dict.items():
-        df.to_excel(writer, sheet_name=table)
-
-    writer.save()
 
 
 if __name__ == '__main__':
